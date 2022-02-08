5cf07431697e029b4bef00a46a3a09a910c13891
Alejandro Mesa <alejandro.suarez@jam3.com>
2022-02-07 23:35:10 -0500

    Initial commit

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,54 @@
+import { useRef } from 'react';
+import Link from 'next/link';
+import classnames from 'classnames';
+
+import styles from './Nav.module.scss';
+
+import Image from '@/components/Image/Image';
+
+import SvgThreeLogo from '@/components/svgs/three-logo.svg';
+import routes from '@/data/routes';
+
+const LINKS = [
+  { href: 'https://jam3.com', label: 'Jam3', file: 'three-logo.jpeg' },
+  { href: 'https://github.com/jam3', label: 'GitHub', file: 'github-icon-64b.png' }
+].map((link) => ({
+  ...link,
+  key: `nav-link-${link.href}-${link.label}`
+}));
+
+function Nav() {
+  const containerRef = useRef<HTMLElement>(null);
+
+  return (
+    <nav className={classnames(styles.Nav)} ref={containerRef}>
+      <div className={styles.wrapper}>
+        <ul className={styles.routes}>
+          <a tabIndex={0} aria-label="Skip to content" className={styles.skipToContent} href="#start-of-content">
+            Skip to content
+          </a>
+          {Object.values(routes).map(({ path, title }) => (
+            <li key={path}>
+              <Link href={path}>
+                <a aria-label="Home">{path === '/' ? <SvgThreeLogo className={styles.threeLogo} /> : title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <ul className={styles.links}>
+          {LINKS.map(({ key, href, label, file }) => (
+            <li key={key}>
+              <a href={href} target="_blank" rel="noopener noreferrer" aria-label={label}>
+                <Image src={file} alt={label} />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <section aria-hidden="true" id="start-of-content"></section>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { AppProps } from 'next/app';
+import { browser, device } from '@jam3/detect';
+import classnames from 'classnames';
+import 'normalize.css';
+import '@/utils/why-did-you-render';
+
+import '@/styles/global.scss';
+
+import Layout from '@/components/Layout/Layout';
+
+import gsapInit from '@/utils/gsap-init';
+
+import { store } from '@/redux';
+
+const isBrowser = typeof window !== 'undefined';
+
+if (isBrowser) {
+  require('default-passive-events');
+  require('focus-visible');
+  gsapInit();
+}
+
+// This default export is required in a new `pages/_app.js` file.
+function App({ Component, pageProps }: AppProps) {
+  const { isUnsupported, ...componentProps } = pageProps;
+
+  useEffect(() => {
+    if (isBrowser) {
+      if (process.env.NODE_ENV !== 'production' && window.location.href.indexOf('?nostat') === -1) {
+        require('@jam3/stats')();
+      }
+
+      document.body.className = `${document.body.className} ${classnames(device.type, browser.name, {
+        'touch-device': device.touch
+      })}`.trim();
+    }
+  }, []);
+
+  if (isUnsupported) {
+    return <Component {...componentProps} />;
+  }
+
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...componentProps} />
+      </Layout>
+    </Provider>
+  );
+}
+
+export default App;
